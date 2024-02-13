5895e653d8876da69ece0317dbdfcc8e63a77489
Muhammad Nur <sholihuddinnur@gmail.com>
2024-02-13 20:55:56 +0700

    Add files via upload

---
+++ manage-audio.js
@@ -0,0 +1,89 @@
+// audio recorder
+let recorder, audio_stream;
+const recordButton = document.getElementById("recordButton");
+recordButton.addEventListener("mousedown", startRecording);
+recordButton.addEventListener("mouseup", stopRecording);
+
+
+// stop recording
+const stopButton = document.getElementById("stopButton");
+// stopButton.addEventListener("click", stopRecording);
+// stopButton.disabled = true;
+
+// set preview
+const preview = document.getElementById("audio-playback");
+
+// set download button event
+const downloadAudio = document.getElementById("downloadButton");
+// downloadAudio.addEventListener("click", downloadRecording);
+// $("#recordButton")
+//     .on( "mousedown", function() {
+//     startRecording();
+//   } )
+//     .on( "mouseup", function() {
+//     stopRecording();
+//     downloadRecording();
+//   } )
+
+function startRecording() {
+    // button settings
+    // recordButton.disabled = true;
+    // recordButton.innerText = "Recording..."
+    // $("#recordButton").addClass("button-animate");
+
+    // $("#stopButton").removeClass("inactive");
+    // stopButton.disabled = false;
+
+
+    // if (!$("#audio-playback").hasClass("hidden")) {
+        // $("#audio-playback").addClass("hidden")
+    // };
+
+    // if (!$("#downloadContainer").hasClass("hidden")) {
+        // $("#downloadContainer").addClass("hidden")
+    // };
+
+    navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(function (stream) {
+            audio_stream = stream;
+            recorder = new MediaRecorder(stream);
+
+            // when there is data, compile into object for preview src
+            recorder.ondataavailable = function (e) {
+                const url = URL.createObjectURL(e.data);
+                preview.src = url;
+
+                // set link href as blob url, replaced instantly if re-recorded
+                downloadAudio.href = url;
+            };
+            recorder.start();
+
+            timeout_status = setTimeout(function () {
+                console.log("5 min timeout");
+                stopRecording();
+            }, 300000);
+        });
+}
+
+function stopRecording() {
+    recorder.stop();
+    audio_stream.getAudioTracks()[0].stop();
+    downloadRecording();
+    // buttons reset
+    // recordButton.disabled = false;
+    // recordButton.innerText = "Redo Recording"
+    // $("#recordButton").removeClass("button-animate");
+
+    // $("#stopButton").addClass("inactive");
+    // stopButton.disabled = true;
+
+    // $("#audio-playback").removeClass("hidden");
+
+    // $("#downloadContainer").removeClass("hidden");
+}
+
+function downloadRecording(){
+    var name = new Date();
+    var res = name.toISOString().slice(0,10)
+    downloadAudio.download = res + '.wav';
+}
